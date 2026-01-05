cc0da5f6df206e34de11a045284f2a4ad4f01172
AkaSokuro <sokuroaka@gmail.com>
2026-01-06 01:28:28 +0700

    Add loop thread for delayed patches

--- dllmain.cpp
+++ dllmain.cpp
@@ -12,6 +12,21 @@
 #include "src/patches/MatchEndCrashFix.h"
 #include "src/patches/DisableBackgroundVideo.h"
 
+PatchManager* g_patchManager = nullptr;
+HANDLE g_monitorThread = NULL;
+bool g_shouldExit = false;
+
+DWORD WINAPI MonitorThread(LPVOID param) {
+    PatchManager* mgr = (PatchManager*)param;
+
+    while (!g_shouldExit) {
+        Sleep(1000);
+        mgr->ApplyDelayedPatches();
+    }
+
+    return 0;
+}
+
 void InitializePatches(HMODULE hModule) {
     char dllPath[MAX_PATH];
     GetModuleFileNameA(hModule, dllPath, MAX_PATH);
@@ -32,21 +47,46 @@ void InitializePatches(HMODULE hModule) {
     HANDLE hProcess = GetCurrentProcess();
     uintptr_t baseAddress = (uintptr_t)GetModuleHandle(NULL);
 
-    PatchManager manager(hProcess, baseAddress, iniPath);
+    g_patchManager = new PatchManager(hProcess, baseAddress, iniPath);
 
-    manager.RegisterPatch<AffinityFixPatch>();
-    manager.RegisterPatch<GamePriorityPatch>();
-    manager.RegisterPatch<FriendlistPatch>();
-    manager.RegisterPatch<MatchEndCrashFixPatch>();
-	manager.RegisterPatch<DisableBackgroundVideoPatch>();
+    g_patchManager->RegisterPatch<AffinityFixPatch>();
+    g_patchManager->RegisterPatch<GamePriorityPatch>();
+    g_patchManager->RegisterPatch<FriendlistPatch>();
+    g_patchManager->RegisterPatch<MatchEndCrashFixPatch>();
+    g_patchManager->RegisterPatch<DisableBackgroundVideoPatch>();
 
-    manager.ApplyAllPatches();
+    g_patchManager->ApplyAllPatches();
+
+    g_monitorThread = CreateThread(NULL, 0, MonitorThread, g_patchManager, 0, NULL);
+}
+
+void CleanupPatches() {
+    g_shouldExit = true;
+
+    if (g_monitorThread) {
+        WaitForSingleObject(g_monitorThread, 2000);
+        CloseHandle(g_monitorThread);
+        g_monitorThread = NULL;
+    }
+
+    if (g_patchManager) {
+        delete g_patchManager;
+        g_patchManager = nullptr;
+    }
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+    switch (ul_reason_for_call) {
+
+    case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hModule);
         InitializePatches(hModule);
+        break;
+
+    case DLL_PROCESS_DETACH:
+        CleanupPatches();
+        break;
     }
+
     return TRUE;
 }
